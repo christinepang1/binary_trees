995379d25deb94a5c37b6793e4d58bd8e8961b3d
christinepang1 <christinezpang@gmail.com>
2020-03-13 20:58:37 +0000

    Changed int to size_t

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,7 +27,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int ileft, iright;
+	size_t ileft, iright;
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
